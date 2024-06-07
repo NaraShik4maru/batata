033a0e65341a48fa2075213991494cf6cf87969a
NaraShik4maru <joaopedrosoarespereira1801@gmail.com>
2024-06-07 16:41:56 -0300

    batata2

--- batata.js
+++ batata.js
@@ -1,1 +1,9 @@
 console.log("Aula de git");
+class Batata{
+    constructor(tipo,capacidade){
+        this.tipo = tipo;
+        this.capacidade = capacidade
+    }
+}
+
+const batata1 = new Batata("batata",5);
